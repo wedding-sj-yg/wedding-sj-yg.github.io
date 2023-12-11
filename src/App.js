623afd15a62a818bda943a6e871c5bd50c7a1bdf
ykwimk <ykwimk@gmail.com>
2023-12-11 10:13:40 +0900

    Feat: 스크롤 섹션 및 lottie animation 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 import Lottie from 'lottie-react';
 import scrollDownLottie from './lotties/animation-scroll-down.json';
-import { useEffect, useState } from 'react';
+import ani from './lotties/ani.json';
+import { useEffect, useRef, useState } from 'react';
+
+const SCROLL_SECTION_LENGTH = 4;
 
 function App() {
-  const [isFadeIn, setIsFadeIn] = useState(false);
+  const scrollRef = useRef(null);
+
+  const [animation, setAnimation] = useState(null);
+
+  const handleScroll = (e) => {
+    const elementHeight = scrollRef.current.clientHeight - window.innerHeight;
+    const sectionHeight = elementHeight / SCROLL_SECTION_LENGTH;
+
+    if (window.scrollY >= sectionHeight && window.scrollY < sectionHeight * 2) {
+      setAnimation('FIRST');
+    } else if (
+      window.scrollY >= sectionHeight * 2 &&
+      window.scrollY < sectionHeight * 3
+    ) {
+      setAnimation('SECOND');
+    } else if (window.scrollY >= sectionHeight * 3) {
+      setAnimation('THIRD');
+    } else {
+      setAnimation(null);
+    }
+  };
 
   useEffect(() => {
-    setIsFadeIn(true);
+    window.scrollTo(0, 0);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
     <div css={root}>
-      <section css={section}>
+      <section ref={scrollRef} css={scrollSection}>
         <div css={content}>
-          <div css={[imageWrapper]}>
-            <div css={[imageContent, isFadeIn && fadeIn]}>
+          <div css={[contentWrapper, imageWrapper]}>
+            <div css={[imageContent, appearFromBottomAnimation]}>
               <img
                 src="/images/wedding-invitation-image.png"
                 alt="청첩장 이미지"
@@ -24,9 +52,72 @@ function App() {
               />
             </div>
           </div>
+          <div
+            css={[
+              contentWrapper,
+              animation === 'FIRST'
+                ? headBlackBackground
+                : animation === 'SECOND' || animation === 'THIRD'
+                  ? headWhiteBackground
+                  : headBackgroundReverse,
+            ]}
+          >
+            <h1 css={[head, animation === 'FIRST' && headTransition]}>
+              배우자가 되겠습니다.
+            </h1>
+          </div>
+          <div
+            css={[
+              contentWrapper,
+              animation === 'SECOND'
+                ? headWhiteBackground
+                : headBackgroundReverse,
+            ]}
+          >
+            <h2
+              css={[
+                head,
+                multiLineTextWithBlackColor,
+                animation === 'SECOND' && headTransition,
+              ]}
+            >
+              아내에게 배우자는 마음과
+              <br />
+              남편에게 배우자는 자세와
+              <br />
+              서로에게 배우자는 의지로
+            </h2>
+          </div>
+          <div
+            css={[
+              contentWrapper,
+              animation === 'THIRD'
+                ? headWhiteBackground
+                : headBackgroundReverse,
+            ]}
+          >
+            <h2
+              css={[
+                head,
+                multiLineTextWithBlackColor,
+                animation === 'THIRD' && headTransition,
+              ]}
+            >
+              한평생 서로에게
+              <br />
+              가장 좋은 배우자가 되겠습니다
+              <br />
+              저희의 배움을 응원해주세요
+              <br />
+              <Lottie
+                animationData={ani}
+                style={{ width: '70px', margin: '0 auto' }}
+              />
+            </h2>
+          </div>
         </div>
       </section>
-      <div css={[scroll, isFadeIn && fadeIn]}>
+      <div css={[scroll, appearFromBottomAnimation]}>
         <Lottie animationData={scrollDownLottie} style={{ width: '70px' }} />
       </div>
     </div>
@@ -38,26 +129,25 @@ const root = css`
   height: 100%;
 `;
 
-const section = css`
+const scrollSection = css`
   position: relative;
   width: 100%;
-  height: 300vh;
-  box-sizing: border-box;
+  height: 400vh;
 `;
 
 const content = css`
   position: sticky;
   top: 0;
+  width: 100%;
   height: 100vh;
 `;
 
-const imageWrapper = css`
+const contentWrapper = css`
   position: absolute;
   top: 0;
   left: 50%;
-  margin-left: -160px;
+  transform: translateX(-50%);
   width: 100%;
-  max-width: 320px;
   height: 100vh;
   display: flex;
   flex-direction: column;
@@ -65,18 +155,16 @@ const imageWrapper = css`
   align-items: center;
 `;
 
+const imageWrapper = css`
+  max-width: 320px;
+  margin-left: -160px;
+  transform: translateX(0%);
+`;
+
 const imageContent = css`
-  transition: all 0.4s 1s;
-  transform: translateY(10px);
-  opacity: 0;
   box-shadow:
     rgba(0, 0, 0, 0.15) 0px 15px 25px,
     rgba(0, 0, 0, 0.05) 0px 5px 10px;
-`;
-
-const fadeIn = css`
-  opacity: 1;
-  transform: translateY(0px);
 `;
 
 const scroll = css`
@@ -84,8 +172,54 @@ const scroll = css`
   bottom: 0;
   left: 50%;
   margin-left: -35px;
+`;
+
+const appearFromBottom = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+`;
+
+const appearFromBottomAnimation = css`
+  animation: ${appearFromBottom} 1s ease;
+`;
+
+const headBlackBackground = css`
+  background: #000;
+  transition: all 0.4s;
+`;
+
+const headWhiteBackground = css`
+  background: #fff;
+  transition: all 0.4s;
+`;
+
+const headBackgroundReverse = css`
+  background: transparent;
+  transition: all 0.4s;
+`;
+
+const head = css`
+  text-align: center;
   opacity: 0;
-  transition: all 0.4s 1.5s;
+  transform: translateY(10px);
+  transition: all 0.6s;
+  color: #fff;
+`;
+
+const multiLineTextWithBlackColor = css`
+  color: #000;
+  line-height: 1.5;
+`;
+
+const headTransition = css`
+  opacity: 1;
+  transform: translateY(0px);
 `;
 
 export default App;
