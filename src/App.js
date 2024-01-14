8110ee6a2ba8b7534e60c8367aa64de2e65f6137
ykwimk <ykwimk@gmail.com>
2024-01-14 19:54:00 +0900

    Fix: hashRouter로 교체

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import Main from './pages/Main';
 import Senior from './pages/Senior';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/senior" element={<Senior />} />
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/senior" element={<Senior />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
